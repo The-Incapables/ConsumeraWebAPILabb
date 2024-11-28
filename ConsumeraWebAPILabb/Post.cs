2728e20766cf71e59767d15ca713c6d3fad80cbd
OlliCupitt <ollicupitt1999@gmail.com>
2024-11-28 12:12:26 +0100

    Add project files.

---
+++ ConsumeraWebAPILabb/Post.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+
+
+
+public class Post
+{
+    [property: JsonPropertyName("Name")]
+    public int Name { get; set; }
+
+    [property: JsonPropertyName("Description")]
+    public int Description { get; set; }
+
+    [property: JsonPropertyName("HTML_URL")]
+    public string HTML_URL { get; set; }
+
+    [property: JsonPropertyName("Homepage")]
+    public string Homepage { get; set; }
+
+    [property: JsonPropertyName("Watchers")]
+    public string Watchers { get; set; }
+
+    [property: JsonPropertyName("Pushed_at")]
+    public string Pushed_at { get; set; }
+    public override string ToString()
+    {
+        return $"Post Name:{this.Name} \n Description:{this.Description}, \n HTML_URL:{this.HTML_URL}," +
+            $" \n Homepage:{this.Homepage},  \n Watchers:{this.Watchers},  \n Pushed_at:{this.Homepage}";
+    }
+
+}
+
+
---
+++ ConsumeraWebAPILabb/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Text.Json;
+using System.Net.Http.Json;
+using HttpClient client = new()
+{
+    BaseAddress = new Uri("https://api.github.com/orgs/dotnet/repos")
+};
+
+Console.WriteLine("Retrieve Post");
+var post = await GetPostAsync(client);
+
+
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine($"{post[i]}\n");
+}
+
+
+static async Task<List<Post>> GetPostAsync(HttpClient client)
+{
+    await using Stream stream = await client.GetStreamAsync("posts");
+    var result = await JsonSerializer.DeserializeAsync<List<Post>>(stream);
+
+    return result ?? new();
+}
+
+
+
+static async Task<Post> SendPostAsync(HttpClient client, Post post)
+{
+    string jsonString = JsonSerializer.Serialize(post);
+    HttpContent content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+    var response = await client.PostAsync("/posts", content);
+
+    response.EnsureSuccessStatusCode();
+    var result = await response.Content.ReadFromJsonAsync<Post>();
+    return result;
+}
